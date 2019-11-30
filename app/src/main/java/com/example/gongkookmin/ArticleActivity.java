e2232e03ec022163c0e9915ff5fce7870bb6d3e8
Jaewook Lee <swljw@kookmin.ac.kr>
2019-11-30 11:00:09 +0900

    Sending string and bitmap data from MainActivity to ArticleActivity

--- app/src/main/java/com/example/gongkookmin/ArticleActivity.java
+++ app/src/main/java/com/example/gongkookmin/ArticleActivity.java
@@ -1,13 +1,90 @@
+/* 작성자 : 이재욱
+* 작성 일자 : 2019년 11월 29일 2시 30분 */
+
 package com.example.gongkookmin;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class ArticleActivity extends AppCompatActivity {
 
+    RecyclerView pictureListView;
+    Toolbar toolbar;
+    ActionBar actionBar;
+    ArrayList<Bitmap> bitmapImages = new ArrayList<Bitmap>();
+
+    /* MainActivity 에서 보낸 데이터틀을 Intent를 통해서 받고 activity_article 위에 띄운다. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+
+        Intent intent = getIntent();
+        String authorStr = intent.getStringExtra("author");
+
+        /* Convert byte[] to Bitmap */
+        byte[] byteArray =  intent.getByteArrayExtra("iconBitmap");
+        Bitmap iconBitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        bitmapImages.add(iconBitmap);
+
+        toolbar = findViewById(R.id.toolbar_article);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle(authorStr);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        /* 될까 싶어서 아무 오픈 카톡 주소를 넣어봤지만 작성자의 AVD에 카톡이 없어서 그런지
+        * 제대로 확인이 되지 않음. */
+        Button btnKakaoLink = (Button) findViewById(R.id.btnOpenKakaoLink);    // 버튼을 누르면 오픈 카톡방으로 연결
+        btnKakaoLink.setOnClickListener(new View.OnClickListener() {    // 버튼 클릭 시 오픈 카톡 채팅방으로 연결.
+            @Override
+            public void onClick(View v) {
+                String testLink = "https://open.kakao.com/o/sXFmeQO";
+                Intent intent_link = new Intent(Intent.ACTION_VIEW, Uri.parse(testLink));
+                startActivity(intent_link);
+            }
+        });
+
+        initPicureList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.piclistview_appbar_action,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:    // actionBar 안에 있는 뒤로 가기 화살표를 누르면 액티비티 종료.
+                finish();
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void initPicureList() {
+        pictureListView = (RecyclerView) findViewById(R.id.pictureViewerListView);
+        pictureListView.setLayoutManager(new LinearLayoutManager(this));
+        PictureListViewAdapter picListAdapter = new PictureListViewAdapter(getApplicationContext(), bitmapImages, 1);
+
+        pictureListView.setAdapter(picListAdapter);
     }
 }
--- app/src/main/java/com/example/gongkookmin/MainActivity.java
+++ app/src/main/java/com/example/gongkookmin/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gongkookmin;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -12,14 +16,18 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SwipeRefreshLayout.OnRefreshListener, ListView.OnItemClickListener{
@@ -99,19 +107,33 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onRefresh() {
 
         // TODO
-
         swipeRefreshLayout.setRefreshing(false);
     }
 
+    /* 작성자 : 이재욱
+       업데이트 : 2019년 11월 29일 2시 30분
+       작성자 이름과 사진을 인텐트를 통해서 ArticleActivity로 보낼 수 있게 기능을 추가하였다. */
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {    // item을 클릭하면 ArticleActivity로 넘어간다.
 
-        /*UserArticlesListViewItem item = (UserArticlesListViewItem) adapterView.getItemAtPosition(i);
-        String titleStr = item.getTitle();
+        UserArticlesListViewItem item = (UserArticlesListViewItem) adapterView.getItemAtPosition(i);
         String authorStr = item.getAuthor();
-        Drawable iconDrawable = item.getIcon();*/
+        String titleStr = item.getTitle();
+        Drawable iconDrawable = item.getIcon();
+
+        /* Change drawable object to bitmap object for sending via intent */
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Bitmap iconBitmap = ((BitmapDrawable) iconDrawable).getBitmap();
+        float scale = (float) (1024 / (float)iconBitmap.getWidth());
+        int width = (int) (iconBitmap.getWidth() * scale);
+        int height = (int) (iconBitmap.getHeight() * scale);
+        Bitmap resizedIcon = Bitmap.createScaledBitmap(iconBitmap, width, height, true);
+        resizedIcon.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
 
         Intent intent = new Intent(getApplication(), ArticleActivity.class);
+        intent.putExtra("author", authorStr);
+        intent.putExtra("iconBitmap", byteArray);
         startActivity(intent);
 
     }
@@ -136,12 +158,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         listView = (ListView) findViewById(R.id.articlesListView);
         ListViewAdapter adapter = new ListViewAdapter();
-        listView.setAdapter(adapter);
 
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.shark), "Example 1", "Mr. A",new Date());
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.nurse),  "Example 2", "Mr. B",new Date());
         adapter.addItem(ContextCompat.getDrawable(this, R.drawable.coffee), "Example 3", "Ms. C", new Date());
 
+        listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
         swipeRefreshLayout = findViewById(R.id.swipelayout);
         swipeRefreshLayout.setOnRefreshListener(this);
